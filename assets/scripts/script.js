c6a36a45df84c95c4a62f09fa1b699fa24be47c8
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-07 21:31:22 -0700

    Deleted Old comments, added local storage saving

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,27 +1,6 @@
-// GAME PLAN
-// Create an object that contains all the information we need per ROW
-// Object information to contain per row:
-// Time: ex. 9am, 2pm
-// Color: grey = past, blue = current, green = future
-// Text: event text content information
-// Index:
-// 9am  [0]
-// 10am [1]
-// 11am [2]
-// 12pm [3]
-// 1pm  [4]
-// 2pm  [5]
-// 3pm  [6]
-// 4pm  [7]
-// 5pm  [8]
-
-// $("button").click(function(){
-//     $("p:first").addClass("intro");
-// });
-
 var currentDate = moment();
-var hourTimes =         ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm"];
-var hourTextContent =   [   "",     "",     "",     "",    "",    "",    "",    "",    "",    "",    "", ""];
+var hourTimes =         ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm"];
+var hourTextContent =   [   "",     "",     "",     "",    "",    "",    "",    "",    "",    "",    "", "", "", ""];
 
 $("#currentDay").text(currentDate.format("MMM Do, YYYY"));
 // $(".container").append("<div class='row col-12 form-group'>");
@@ -35,18 +14,15 @@ function generateBlocks() {
             $(".row").last().append("<p class='hour col-2'>");
             $(".hour").last().text(hourTimes[i]);
             $(".row").last().append("<textarea id='hour"+hourTimes[i]+"'class='form-control col-8'>");
-            $(".row").last().append("<button class='saveBtn col-2'>");
+            $(".row").last().append("<button id='"+i+"'class='saveBtn col-2'>");
             $(".saveBtn").last().text("Save");
     }
 }
-
+// color coat the rows appropriate for the time
 function colorRows() {
     var currentHour = moment().format("ha");
-    var curH = moment().format("h");
-    // if (hourTimes.includes($(".hour" + currentHour).attr('id'))) {
-    //     console.log($(".hour" + currentHour));
-    // }
-    console.log(curH);
+    // var curH = moment().format("h");
+
     for (var i = 0; i < hourTimes.length; i++) {
         if (i < hourTimes.indexOf(currentHour)) {
             // past
@@ -60,45 +36,22 @@ function colorRows() {
             $("#hour" + hourTimes[i]).addClass("future");
         }
     }
-    
-    // console.log($("p")[2].attributes.[2]);
 }
-// formGroup = $(".form-group");
-
-// $(formGroup).click(function(event) {
-//     event.stopPropagation();
-//     event.preventDefault();
-//     console.log($(event.currentTarget));
-// });
-
-    // event.preventDefault();
-    // event.stopPropagation();
 
 generateBlocks();
 colorRows();
 
-// 
 
-// formGroup.on("click", saveItem);
-// $(event.currentTarget)
+// on save click, save information to local storage
+$(".saveBtn").click(function(event) { 
+    event.stopPropagation();
+    event.preventDefault();
+    // get the id of the button pressed and store it in this value
+    var saveButton = $(this).attr("id");
+    
+    hourTextContent[saveButton] = $("#hour"+hourTimes[saveButton]).val();
+    localStorage.setItem(hourTimes[saveButton], hourTextContent[saveButton]);
 
-// var currentHour = moment().format("ha");
-// console.log(hourTimes.indexOf(currentHour))
-
-
-// $("hour" + currentHour).attr("class", "present");
-// console.log(colorRows());
-
-
-// function remove(event) {
-//     var
-// }
-// event.currentTarget;
-
-// $(".row").append("<p class='hour col-2'>");
-// var timeVis = $("p").attr("id",hourTimes[0]);
-// rowVal.attr("id",hourTimes[0]);
-
-// .attr('id', 'taco');
-
-// var valueThing = $("<div>").addClass("");
+    console.log(saveButton);
+    console.log(hourTextContent[saveButton]);
+});
